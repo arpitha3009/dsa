90bba3d29ce1143da6d072943415669ada8351a0
arpitha3009 <arpithagowda1971@gmail.com>
2026-04-17 19:33:58 +0530

    day1

---
+++ src/array/sumofelement.java
@@ -0,0 +1,14 @@
+package array;
+
+public class sumofelement {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,3};
+        int sum = 0;
+        for(int i =0 ;i<arr.length;i++) {
+            sum += arr[i];
+
+
+        }
+        System.out.println(sum);
+    }
+}
