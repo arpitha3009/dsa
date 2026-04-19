cc2722d08700bfaa0d86e2120b5e7c0769491a4d
arpitha3009 <arpithagowda1971@gmail.com>
2026-04-19 20:12:51 +0530

    day2

---
+++ src/array/productOfArray.java
@@ -0,0 +1,13 @@
+package array;
+
+public class productOfArray {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 1, 1};
+        int product =1;
+        for(int i =0;i<arr.length;i++) {
+            product *= arr[i];
+        }
+        System.out.println(product);
+
+    }
+}
