38eff7f7fd789f8e4fea7ce5eca9119d7655c8c1
arpitha3009 <arpithagowda1971@gmail.com>
2026-04-19 19:33:54 +0530

    day2

--- src/array/shallowCopyDeepCopy.java
+++ src/array/shallowCopyDeepCopy.java
@@ -1,12 +1,21 @@
 package array;
 
+import java.util.Arrays;
+
 public class shallowCopyDeepCopy {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 7, 6};
+        // shallow copy
         int[] x = arr;
         x[2]=100;
         System.out.println(arr[2]);
         System.out.println(x[2]);
+
+        //deep copy
+        int[] y = Arrays.copyOf(arr,arr.length);
+        y[2]=2;
+        System.out.println(arr[2]);
+        System.out.println(y[2]);
 
     }
 }
