4d32473d19057fa8cc38600fa4ec2e1f7d69d6ff
arpitha3009 <arpithagowda1971@gmail.com>
2026-04-20 23:26:53 +0530

    day3

--- src/array/productOfArray.java
+++ src/array/productOfArray.java
@@ -2,7 +2,7 @@ package array;
 
 public class productOfArray {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 1, 1};
+        int[] arr = {1, 2, 1, 1, 1};
         int product =1;
         for(int i =0;i<arr.length;i++) {
             product *= arr[i];
